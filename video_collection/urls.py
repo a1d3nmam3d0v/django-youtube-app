5658d726ff4021436b718f68a99f2e1e49418dd3
a1d3n <$>
2022-11-28 12:27:57 -0600

    link from video list goes to a video detail page for each video

--- video_collection/urls.py
+++ video_collection/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("add", views.add, name="add_video"),
     path("video_list", views.video_list, name="video_list"),
+    # Lab part 2 -
+    # Create a new page that displays all the data about one video.
+    path("video/<int:video_pk>", views.video_details, name="video_details"),
 ]
